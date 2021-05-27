22f3bdc46e700b7671b4943cd1bd5d5b9a1d59f8
mattiadebonis <mattia.debonis@gmail.com>
2021-05-27 17:35:45 +0200

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 var app = new Vue({
-    el: "#root"
+    el: "#root",
+    data:{
+        mailUsers: []
+    },
+    mounted: function(){
+        for (var i =0; i<10; i++){
+            axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+
+                .then((result) => {                    
+                        const mail = result.data.response;
+                        this.mailUsers.push(mail);                        
+                });
+        }
+    }
 });
